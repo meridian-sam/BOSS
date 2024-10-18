a9233cfe719afee8f674306da4821522359c290f
Nicolae Mihalache <xpromache@gmail.com>
2024-10-18 19:40:43 +0200

    script to duplicate UDP packets

---
+++ duplicate-udp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Duplicate the UDP packets received by COSMOS so that both Yamcs and COSMOS can see the same TM
+
+c1=cosmos_openc3-operator_1
+c2=yamcs-operator_1
+
+# Get the IPs of the containers
+ip1=`docker inspect -f '{{.NetworkSettings.Networks.nos3_sc_1.IPAddress}}' $c1`
+ip2=`docker inspect -f '{{.NetworkSettings.Networks.nos3_sc_1.IPAddress}}' $c2`
+
+
+if [[ -z $ip1 || -z $ip2 ]]; then
+  echo "Error: Unable to get IP addresses of the containers."
+  exit 1
+fi
+
+pid1=$(docker inspect -f '{{.State.Pid}}' $c1)
+pid2=$(docker inspect -f '{{.State.Pid}}' $c2)
+
+if [[ -z $pid1 || -z $pid2 ]]; then
+  echo "Error: Unable to get PIDs of the containers."
+  exit 1
+fi
+
+echo "$c1:  $ip1 $pid1"
+echo "$c2:  $ip2 $pid2"
+
+port=5013
+
+for port in 5013 5111 6011; do
+   echo "duplicating port $port"
+   nsenter -t $pid2 -n iptables -t nat -A PREROUTING -p udp --dport $port -j DNAT --to-destination $ip2
+   nsenter -t $pid1 -n iptables -t mangle -A PREROUTING -p udp --dport $port -j TEE --gateway $ip2
+done 
+
